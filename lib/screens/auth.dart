48c211db7dcc19d9dcf22a83aea41dd8c03d1c9a
RoyalBosS <ayushmunot921@gmail.com>
2023-12-22 14:52:55 +0530

    firebase storage and firestore setup

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -13,20 +18,26 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  var _isLogin = true;
+  var _isLogin = false;
   final _form = GlobalKey<FormState>();
   var _enteredEmail = "";
   var _enteredPassword = "";
+  var _enteredUsername = "";
+  File? _selectedImage;
+  var _isAuthenticating = false;
 
   void _submit() async {
     final isValid = _form.currentState!.validate();
-    if (!isValid) {
+    if (!isValid || (!_isLogin && _selectedImage == null)) {
       return;
     }
 
     _form.currentState!.save();
 
     try {
+      setState(() {
+        _isAuthenticating = true;
+      });
       if (_isLogin) {
         final userCredential = await _firebase.signInWithEmailAndPassword(
           email: _enteredEmail,
@@ -37,6 +48,27 @@ class _AuthScreenState extends State<AuthScreen> {
           email: _enteredEmail,
           password: _enteredPassword,
         );
+
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child("user_images")
+            .child("${userCredential.user!.uid}.jpg");
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+
+        await FirebaseFirestore.instance
+            .collection("users")
+            .doc(userCredential.user!.uid)
+            .set({
+          "username": _enteredUsername,
+          "email": _enteredEmail,
+          "image_url": imageUrl,
+        });
+
+        setState(() {
+          _isAuthenticating = true;
+        });
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == "email-already-in-use") {}
@@ -47,6 +79,9 @@ class _AuthScreenState extends State<AuthScreen> {
             content: Text(error.message ?? "Authentication failed."),
           ),
         );
+        setState(() {
+          _isAuthenticating = true;
+        });
       }
     }
   }
@@ -80,6 +115,12 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
+                      if (!_isLogin)
+                        UserImagePicker(
+                          onPickImage: (pickedImage) {
+                            _selectedImage = pickedImage;
+                          },
+                        ),
                       TextFormField(
                         decoration: const InputDecoration(
                           labelText: "Email Address",
@@ -99,6 +140,24 @@ class _AuthScreenState extends State<AuthScreen> {
                           _enteredEmail = value!;
                         },
                       ),
+                      if (!_isLogin)
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Username",
+                          ),
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value == null || value.trim().length < 4) {
+                              return 'Please enter at lease 4 characters.';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _enteredUsername = value!;
+                          },
+                        ),
                       TextFormField(
                         decoration: const InputDecoration(
                           labelText: "Password",
@@ -115,24 +174,27 @@ class _AuthScreenState extends State<AuthScreen> {
                         },
                       ),
                       const SizedBox(height: 12),
-                      ElevatedButton(
-                        onPressed: _submit,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primaryContainer,
+                      if (_isAuthenticating) const CircularProgressIndicator(),
+                      if (!_isAuthenticating)
+                        ElevatedButton(
+                          onPressed: _submit,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primaryContainer,
+                          ),
+                          child: Text(_isLogin ? "Login" : "Sign Up"),
                         ),
-                        child: Text(_isLogin ? "Login" : "Sign Up"),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          setState(() {
-                            _isLogin = !_isLogin;
-                          });
-                        },
-                        child: Text(_isLogin
-                            ? "Create an account"
-                            : "I already have an account. Login."),
-                      ),
+                      if (!_isAuthenticating)
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(_isLogin
+                              ? "Create an account"
+                              : "I already have an account. Login."),
+                        ),
                     ],
                   )),
             )),
